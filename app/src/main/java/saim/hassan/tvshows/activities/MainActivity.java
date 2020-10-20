9dac1e84f5618830f0109ab8bdcbf641a3417632
SAIM <saimh355@gmail.com>
2020-10-20 18:03:08 +0500

    Setup Retrofit and TV Shows API Integration with the android app

--- app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package saim.hassan.tvshows;
+package saim.hassan.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import saim.hassan.tvshows.R;
 
 public class MainActivity extends AppCompatActivity {
 
