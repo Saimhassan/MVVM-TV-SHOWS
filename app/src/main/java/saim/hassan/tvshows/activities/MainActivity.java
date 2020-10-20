2712339ecf0c16681fccebd4d2b36c200380e342
SAIM <saimh355@gmail.com>
2020-10-20 20:21:43 +0500

    Setup Retrofit and TV Shows API Integration with the android app

--- app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
@@ -1,16 +1,34 @@
 package saim.hassan.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import saim.hassan.tvshows.R;
+import saim.hassan.tvshows.viewmodels.MostPopularTVShowsViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MostPopularTVShowsViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        getMostPopularTVShows();
+    }
+
+    private void getMostPopularTVShows(){
+        viewModel.getMostPopularTVShows(0).observe(this,mostPopularTVShowsResponse->
+                       Toast.makeText(
+                               getApplicationContext(),
+                               "Total Pages: "+mostPopularTVShowsResponse.getTotalPages(),
+                               Toast.LENGTH_SHORT
+                       ).show()
+                );
     }
 }
