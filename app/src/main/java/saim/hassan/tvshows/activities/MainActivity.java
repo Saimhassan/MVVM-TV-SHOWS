7dd3847c6898f628c9c1e6744366890e4b105778
SAIM <saimh355@gmail.com>
2020-10-21 08:56:30 +0500

    Pagination In TV Show List - Endless Recycler View

--- app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package saim.hassan.tvshows.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private List<TVShow> tvShows = new ArrayList<>();
     private TVShowsAdapter tvShowsAdapter;
     private int currentPage = 1;
+    private int totalAvailablePages = 1;
 
 
 
@@ -36,6 +39,18 @@ public class MainActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
         tvShowsAdapter = new TVShowsAdapter(tvShows);
         activityMainBinding.tvShowsRecyclerView.setAdapter(tvShowsAdapter);
+        activityMainBinding.tvShowsRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!activityMainBinding.tvShowsRecyclerView.canScrollVertically(1)){
+                    if (currentPage <= totalAvailablePages){
+                        currentPage += 1;
+                        getMostPopularTVShows();
+                    }
+                }
+            }
+        });
         getMostPopularTVShows();
     }
 
@@ -44,9 +59,11 @@ public class MainActivity extends AppCompatActivity {
         viewModel.getMostPopularTVShows(currentPage).observe(this,mostPopularTVShowsResponse->{
                      toggleLoading();
                      if (mostPopularTVShowsResponse != null){
+                         totalAvailablePages = mostPopularTVShowsResponse.getTotalPages();
                          if (mostPopularTVShowsResponse.getTvShows() != null){
+                             int oldCount = tvShows.size();
                              tvShows.addAll(mostPopularTVShowsResponse.getTvShows());
-                             tvShowsAdapter.notifyDataSetChanged();
+                             tvShowsAdapter.notifyItemRangeInserted(oldCount,tvShows.size());
                          }
                      }
                 });
