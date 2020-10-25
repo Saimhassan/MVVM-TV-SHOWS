28cf637d4e79271c1e241fdf6124384d50b8ec4e
SAIM <saimh355@gmail.com>
2020-10-25 18:02:02 +0500

    TV Shows Details API Integration

--- app/src/main/java/saim/hassan/tvshows/activities/TVShowsDetailActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/TVShowsDetailActivity.java
@@ -1,16 +1,39 @@
 package saim.hassan.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import saim.hassan.tvshows.R;
+import saim.hassan.tvshows.databinding.ActivityTVShowsDetailBinding;
+import saim.hassan.tvshows.viewmodels.TvShowDetailsViewModel;
 
 public class TVShowsDetailActivity extends AppCompatActivity {
+    private ActivityTVShowsDetailBinding activityTVShowsDetailBinding;
+    private TvShowDetailsViewModel tvShowDetailsViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_t_v_shows_detail);
+        activityTVShowsDetailBinding = DataBindingUtil.setContentView(this,R.layout.activity_t_v_shows_detail);
+        doInitaialization();
+    }
+
+    private void doInitaialization(){
+        tvShowDetailsViewModel = new ViewModelProvider(this).get(TvShowDetailsViewModel.class);
+        getTVShowDetails();
+    }
+    private void getTVShowDetails(){
+        activityTVShowsDetailBinding.setIsLoading(true);
+        String tvShowId = String.valueOf(getIntent().getIntExtra("id",-1));
+        tvShowDetailsViewModel.getTVShowDetails(tvShowId).observe(
+                this,tvShowDetailResponse -> {
+                    activityTVShowsDetailBinding.setIsLoading(false);
+                    Toast.makeText(getApplicationContext(),tvShowDetailResponse.getTvShowDetail().getUrl(),Toast.LENGTH_SHORT).show();
+                }
+        );
     }
 }
