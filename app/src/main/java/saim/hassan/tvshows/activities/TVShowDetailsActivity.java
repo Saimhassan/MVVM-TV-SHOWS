9a30cb786ea7cd0c44c2901e95600d80338895b8
SAIM <saimh355@gmail.com>
2020-10-25 13:42:26 +0500

    TV Shows Details API Integration

--- app/src/main/java/saim/hassan/tvshows/activities/TVShowDetailsActivity.java
+++
@@ -1,4 +0,0 @@
-package saim.hassan.tvshows.activities;
-
-public class TVShowDetailsActivity {
-}
---
+++ app/src/main/java/saim/hassan/tvshows/activities/TVShowsDetailActivity.java
@@ -0,0 +1,16 @@
+package saim.hassan.tvshows.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import saim.hassan.tvshows.R;
+
+public class TVShowsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t_v_shows_detail);
+    }
+}
