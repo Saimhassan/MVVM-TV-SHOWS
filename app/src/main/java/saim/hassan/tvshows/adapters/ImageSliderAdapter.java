d44791a8bab718f9ea2de8da8fd0f3f1577af61f
SAIM <saimh355@gmail.com>
2020-10-26 18:21:45 +0500

    Display Image Slider Using ViewPager2 and Data Binding

--- app/src/main/java/saim/hassan/tvshows/adapters/ImageSliderAdapter.java
+++ app/src/main/java/saim/hassan/tvshows/adapters/ImageSliderAdapter.java
@@ -1,4 +1,24 @@
 package saim.hassan.tvshows.adapters;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import saim.hassan.tvshows.databinding.ItemConatinerSliderImageBinding;
+
 public class ImageSliderAdapter {
+    static class ImageSliderViewHolder extends RecyclerView.ViewHolder {
+
+        private ItemConatinerSliderImageBinding itemConatinerSliderImageBinding;
+
+        public ImageSliderViewHolder(ItemConatinerSliderImageBinding itemConatinerSliderImageBinding){
+            super(itemConatinerSliderImageBinding.getRoot());
+            this.itemConatinerSliderImageBinding = itemConatinerSliderImageBinding;
+        }
+
+        public void bindSliderImage(String imageURL){
+            itemConatinerSliderImageBinding.setImageURL(imageURL);
+        }
+    }
 }
