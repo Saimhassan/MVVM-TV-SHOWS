6aa7b180200dacc650e0d4ebad792e0992a480f8
SAIM <saimh355@gmail.com>
2020-10-26 19:08:43 +0500

    Display Image Slider Using ViewPager2 and Data Binding

--- app/src/main/java/saim/hassan/tvshows/activities/TVShowsDetailActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/TVShowsDetailActivity.java
@@ -1,11 +1,15 @@
 package saim.hassan.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import saim.hassan.tvshows.R;
@@ -47,7 +51,25 @@ public class TVShowsDetailActivity extends AppCompatActivity {
         activityTVShowsDetailBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
         activityTVShowsDetailBinding.sliderViewPager.setVisibility(View.VISIBLE);
         activityTVShowsDetailBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicator(sliderImages.length);
     }
 
-    
+    private void setupSliderIndicator(int count){
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8,0,8,0);
+        for (int i = 0; i<indicators.length;i++){
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.background_slider_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTVShowsDetailBinding.layoutSliderIndicator.addView(indicators[i]);
+        }
+        activityTVShowsDetailBinding.layoutSliderIndicator.setVisibility(View.VISIBLE);
+
+    }
 }
