49126460357d82c11ebf22eaf9b02bc604c4952e
SAIM <saimh355@gmail.com>
2020-10-26 18:29:16 +0500

    Display Image Slider Using ViewPager2 and Data Binding

--- app/src/main/java/saim/hassan/tvshows/adapters/ImageSliderAdapter.java
+++ app/src/main/java/saim/hassan/tvshows/adapters/ImageSliderAdapter.java
@@ -1,13 +1,48 @@
 package saim.hassan.tvshows.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import saim.hassan.tvshows.R;
 import saim.hassan.tvshows.databinding.ItemConatinerSliderImageBinding;
 
-public class ImageSliderAdapter {
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.ImageSliderViewHolder>{
+
+    private String sliderImages;
+    private LayoutInflater layoutInflater;
+
+    public ImageSliderAdapter(String sliderImages) {
+        this.sliderImages = sliderImages;
+    }
+
+    @NonNull
+    @Override
+    public ImageSliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (layoutInflater == null)
+        {
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        ItemConatinerSliderImageBinding sliderImageBinding = DataBindingUtil.inflate(
+                layoutInflater, R.layout.item_conatiner_slider_image,parent,false
+        );
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageSliderViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     static class ImageSliderViewHolder extends RecyclerView.ViewHolder {
 
         private ItemConatinerSliderImageBinding itemConatinerSliderImageBinding;
