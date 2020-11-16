f319c52594c9fc923429c19ab085b75aec8e6cb5
SAIM <saimh355@gmail.com>
2020-11-16 11:25:22 +0500

    Room Database Setup & Creating WishList

---
+++ app/src/main/java/saim/hassan/tvshows/activities/WatchListActivity.java
@@ -0,0 +1,16 @@
+package saim.hassan.tvshows.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import saim.hassan.tvshows.R;
+
+public class WatchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_list);
+    }
+}
