24addb093f9ea9ab4f7167323a73ba9f9be1c1e8
SAIM <saimh355@gmail.com>
2020-11-16 17:04:56 +0500

    Room Database Setup & Creating WishList

--- app/src/main/java/saim/hassan/tvshows/activities/WatchListActivity.java
+++ app/src/main/java/saim/hassan/tvshows/activities/WatchListActivity.java
@@ -1,16 +1,29 @@
 package saim.hassan.tvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 
 import saim.hassan.tvshows.R;
+import saim.hassan.tvshows.databinding.ActivityWatchListBinding;
+import saim.hassan.tvshows.viewmodels.WatchListViewModel;
 
 public class WatchListActivity extends AppCompatActivity {
+
+    private ActivityWatchListBinding activityWatchListBinding;
+    private WatchListViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_watch_list);
+        activityWatchListBinding = DataBindingUtil.setContentView(this,R.layout.activity_watch_list);
+        doInitalization();
+    }
+    private void doInitalization(){
+      viewModel = new ViewModelProvider(this).get(WatchListViewModel.class);
+      activityWatchListBinding.imageBack.setOnClickListener(v -> onBackPressed());
     }
 }
