23db3f3a1bd152e0ad112a121ff72a4d55ffc7b1
SAIM <saimh355@gmail.com>
2020-11-16 16:26:37 +0500

    Room Database Setup & Creating WishList

---
+++ app/src/main/java/saim/hassan/tvshows/viewmodels/WatchListViewModel.java
@@ -0,0 +1,26 @@
+package saim.hassan.tvshows.viewmodels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import saim.hassan.tvshows.database.TVShowDatabase;
+import saim.hassan.tvshows.models.TVShow;
+
+public class WatchListViewModel extends AndroidViewModel {
+
+    private TVShowDatabase tvShowDatabase;
+    public WatchListViewModel(@NonNull Application application){
+        super(application);
+        tvShowDatabase = TVShowDatabase.getTvShowDatabase(application);
+    }
+
+    public Flowable<List<TVShow>> loadWatchlist(){
+        return tvShowDatabase.tvShowDao().getWatchList();
+    }
+
+}
